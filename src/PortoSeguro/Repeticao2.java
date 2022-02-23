073bf17f04cb1bde9a44b2e48c52195cccbe1d27
Caroline R <carol.lmbg25@gmail.com>
2022-02-23 17:02:17 -0300

    Tabuada finalizada

--- src/PortoSeguro/Repeticao2.java
+++ src/PortoSeguro/Repeticao2.java
@@ -1,10 +1,39 @@
 package PortoSeguro;
 
+import java.util.Scanner;
+
 public class Repeticao2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//Tabuada
+		//Class 3, 9 de fevereiro de 2022
+		
+		
+		int tabuada,x=1,resultado=0;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		//Qual tabuada meu usuário meu usuário deseja montar?
+		
+		
+		System.out.println("\nQual tabuada você deseja montar?");
+		tabuada = leia.nextInt();
+				
+		
+		/*A diferença entre o while e o do/while é que esse executa primeiro e depois testa. 
+		  Enquanto o while testa e depois executa.	*/
+		
+		
+		do
+		{
+			resultado = x * tabuada;
+			System.out.println("\n"+tabuada+" X "+x+" = "+resultado);
+			x++;
+		}
+		while(x<=10);
+		
 	}
 
 }
