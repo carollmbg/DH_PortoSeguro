4bbdb38407481cade00023252904df17e91cc1bd
Caroline R <carol.lmbg25@gmail.com>
2022-03-25 21:37:46 -0300

    Atualização do eclipse e exercicio de laço incompleto.

--- src/Exercicios/LacoRepeticao1.java
+++ src/Exercicios/LacoRepeticao1.java
@@ -1,5 +1,7 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class LacoRepeticao1 {
 
 	public static void main(String[] args) {
@@ -13,6 +15,27 @@ public class LacoRepeticao1 {
 	C) maior salário;
 	D) percentual de pessoas com salário até R$100,00.
   */
+	
+		
+		Scanner leia = new Scanner(System.in); //entrada de dados
+		
+		int[][] pesquisa = new int[20][3]; //2 dimensões, linha e coluna. 20 posições de linha, 3 posições de coluna. 
+		//(inicia posição 0)
+		
+		 int salario[] = { 150,200,250,650,800,900,1200,10000,6000,2000,3000,1500,2500,450,500,1300,2 }; 
+		 int filhos[] = {  3,5,2,3,2,2,2,0,1,2,3,2,2,0,3,0,2 }; 
+		 String habitante[] = { "Ana","John","Maria","Ananda","Latifa","Bruna","Carol","Dani","Melo","Silva","Manu","Paulo","Rafa",
+				 "Nina","Rene","Lina","Jessy","Tom","Jerry","Bach"}; 
+	      
+		 
+		 for (int x = 0; x <salario.length; x++)
+		 {
+			 System.out.print(salario[x] + " ");
+		 }
+		   
+		
+		
+				
 
 	}
 
