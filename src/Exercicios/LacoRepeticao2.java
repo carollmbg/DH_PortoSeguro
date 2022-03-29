38dcf755fc144e2ddba679a4b88bb66a62a7c049
Caroline R <carol.lmbg25@gmail.com>
2022-03-29 19:32:26 -0300

    Exercicio de laço de repetição. Desenvolver um sistema que efetue a soma de todos os números ímpares que são múltiplos de três e que se encontram no conjunto dos números de 1 até 500. Finalizado

--- src/Exercicios/LacoRepeticao2.java
+++ src/Exercicios/LacoRepeticao2.java
@@ -8,6 +8,20 @@ public class LacoRepeticao2 {
 		/*2- Desenvolver um sistema que efetue a soma de todos os números ímpares que são
 múltiplos de três e que se encontram no conjunto dos números de 1 até 500.
 */
+		
+		
+		int soma = 0;
+	
+		
+		for(int numero=1;numero<=500;numero++)
+		{
+			if(numero % 3 == 0 && numero % 2 != 0)
+			{
+				soma = soma + numero;
+			}
+			
+		}
+		System.out.println(soma);
 
 	}
 
